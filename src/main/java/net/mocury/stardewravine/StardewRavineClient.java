678eaf8c40b12ef5c1eabcc7b3ee4822ab6c8d0e
MocuryMoto <neptunethelatios@gmail.com>
2026-04-26 13:01:04 -0400

    Fixed Cinder Cluster visuals

--- src/main/java/net/mocury/stardewravine/StardewRavineClient.java
+++ src/main/java/net/mocury/stardewravine/StardewRavineClient.java
@@ -1,10 +1,16 @@
 package net.mocury.stardewravine;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.mocury.stardewravine.block.ModBlocks;
 
 public class StardewRavineClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CINDER_CLUSTER, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LARGE_CINDER_BUD, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MEDIUM_CINDER_BUD, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SMALL_CINDER_BUD, RenderLayer.getCutout());
     }
 }
